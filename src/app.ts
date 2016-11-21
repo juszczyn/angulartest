7d1a9679a92a48da9d63d4a1023c823f205ac892
juszczyn <piotr.juszczynski@gmail.com>
2016-11-21 23:42:12 +0100

    wersja

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
+import {AccordionModule} from 'primeng/primeng';  
 
 @Component({
     selector: 'app',
